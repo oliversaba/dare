ed4e958e85b05af93ccaed3a9fbcdbb15d3f9081
oliversaba <69880537+oliversaba@users.noreply.github.com>
2020-08-19 11:20:56 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,133 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("dare")
+    if (Math.randomBoolean()) {
+        if (Math.randomBoolean()) {
+            if (Math.randomBoolean()) {
+                if (Math.randomBoolean()) {
+                    if (Math.randomBoolean()) {
+                        basic.showString("kiss the floor")
+                    } else {
+                        basic.showString("yeel 'missss'")
+                    }
+                    if (Math.randomBoolean()) {
+                        basic.showString("say who you like")
+                    } else {
+                        basic.showString("let me slap you")
+                    }
+                } else {
+                    if (Math.randomBoolean()) {
+                        basic.showString("snap your pen/pencel")
+                    } else {
+                        basic.showString("pic your nose")
+                    }
+                }
+            } else {
+                if (Math.randomBoolean()) {
+                    if (Math.randomBoolean()) {
+                        basic.showString("do the worm")
+                    } else {
+                        basic.showString("do a stupit dance")
+                    }
+                } else {
+                    if (Math.randomBoolean()) {
+                        basic.showString("yell 'niceee'")
+                    } else {
+                        basic.showString("say i am the best")
+                    }
+                }
+            }
+        } else {
+            if (Math.randomBoolean()) {
+                if (Math.randomBoolean()) {
+                    if (Math.randomBoolean()) {
+                        basic.showString("go run out side")
+                    } else {
+                        basic.showString("draw a bird")
+                    }
+                } else {
+                    if (Math.randomBoolean()) {
+                        basic.showString("role again")
+                    } else {
+                        basic.showString("safe")
+                    }
+                }
+            } else {
+                if (Math.randomBoolean()) {
+                    if (Math.randomBoolean()) {
+                        basic.showString("act like a gorila for 30 seconds ")
+                    } else {
+                        basic.showString("ask someone out")
+                    }
+                } else {
+                    if (Math.randomBoolean()) {
+                        basic.showString("be as loud s you can for 10s")
+                    } else {
+                        basic.showString("lick the desk")
+                    }
+                }
+            }
+        }
+    } else {
+        if (Math.randomBoolean()) {
+            if (Math.randomBoolean()) {
+                if (Math.randomBoolean()) {
+                    if (Math.randomBoolean()) {
+                        basic.showString("punch someones lap")
+                    } else {
+                        basic.showString("hide somones laptop")
+                    }
+                } else {
+                    if (Math.randomBoolean()) {
+                        basic.showString("put someone at your desk in a headlock")
+                    } else {
+                        basic.showString("put me in a head lock")
+                    }
+                }
+            } else {
+                if (Math.randomBoolean()) {
+                    if (Math.randomBoolean()) {
+                        basic.showString("let me hit you")
+                    } else {
+                        basic.showString("truth or dare me")
+                    }
+                } else {
+                    if (Math.randomBoolean()) {
+                        basic.showString("say you favourt team")
+                    } else {
+                        basic.showString("safe")
+                    }
+                }
+            }
+        } else {
+            if (Math.randomBoolean()) {
+                if (Math.randomBoolean()) {
+                    if (Math.randomBoolean()) {
+                        basic.showString("sit on the person next to you")
+                    } else {
+                        basic.showString("kick a chair")
+                    }
+                } else {
+                    if (Math.randomBoolean()) {
+                        basic.showString("say sompthing loudly ")
+                    } else {
+                        basic.showString("make a funny face")
+                    }
+                }
+            } else {
+                if (Math.randomBoolean()) {
+                    if (Math.randomBoolean()) {
+                        basic.showString("put the rude finger up at someone ")
+                    } else {
+                        basic.showString("do 20 pushups")
+                    }
+                } else {
+                    if (Math.randomBoolean()) {
+                        basic.showString("will you go out with alex?")
+                    } else {
+                        basic.showString("do 30 pushups")
+                    }
+                }
+            }
+        }
+    }
+})
